82aefb9e03512bcedcda4e5cbce709d2bfde1d64
Abbey Reisle <areisle@gmail.com>
2017-02-18 21:17:11 -0800

    linted my js and added to gitignore for mac

--- scripts/bird-fsm.js
+++ scripts/bird-fsm.js
@@ -1,20 +1,148 @@
-$(document).ready(function(){
-    
+/*global $, console, machina*/
+//given x, find y of given curve
+function newton(x, path) {
+    //pick a starting point
+    //try halfway between x value and total length
+    //since length(x) >= x
+    "use strict";
+    var scale = $(path).parent('svg').width() / path.getBBox().width,
+        end,
+        start,
+        testlength,
+        testpoint;
+    x /= scale;
+    start = x;
+    end = path.getTotalLength();
+
+    //algorithm part
+    //edge cases: at end of path, at start of path
+    while (true) {
+      //try middle of current bounding region
+        testlength = Math.floor((start + end) / 2);
+        testpoint = path.getPointAtLength(testlength);
+        if (testlength === end || testlength === start) {
+            break;
+        } else if (testpoint.x > x) {
+            //guess was too high
+            end = testlength;
+        } else if (testpoint.x < x) {
+            //guess was too low
+            start = testlength;
+        } else {
+            //guess is correct
+            break;
+        }
+    }
+    return testpoint.y * scale;
+}
+
+//get position to shuffle to
+function shuffle(x, step, path, direction) {
+    //move set distance (step) in specified direction
+    //parameters:
+    //x: current x position
+    //step: distance to shift
+    //path: curve to move along
+    //direction: which way to move along curve
+    //returns: 
+    //{x,y}: new position on curve
+    "use strict";
+    var newposition = {};
+    newposition.x = x;
+    //console.log("x is: " + x);
+    if (direction === "left") {
+        //new x value
+        newposition.x -= step;
+        //new y value
+    } else if (direction === "right") {
+        //new x value
+        newposition.x += step;
+
+    } else {
+        //else throw error?d
+        //add one in later
+        console.log("error!");
+        return;
+    }
+    newposition.y = newton(newposition.x, path);
+    return newposition;
+}
+
+//start the shuffling animation in the specified direction
+function startShuffling($thing, direction, path, $birdBefore) {
+    "use strict";
+    $thing.off("webkitAnimationIteration");
+    $birdBefore.off("webkitAnimationIteration");
+    //get next position on curve
+    var position = shuffle($thing.offset().left + 0.5 * $thing.width(), 0.5 * $thing.width(), path, direction),
+        angle,
+        opposite,
+        adjacent;
+    //find the slope of the curve at the birds current position
+    opposite = newton($thing.offset().left, path) - newton($thing.offset().left + $thing.width(), path);
+    adjacent = $thing.width();
+    angle = -1 * Math.atan(opposite / adjacent) / Math.PI * 180;
+    //set the css variables
+    document.documentElement.style.setProperty('--position-x', position.x - 0.5 * $thing.width());
+    document.documentElement.style.setProperty('--position-y', position.y - 0.9 * $thing.width());
+    document.documentElement.style.setProperty('--angle', angle + 'deg');
+    //start the animations
+    $thing.css('animation-play-state', 'running');
+    $birdBefore.css('animation-play-state', 'running');
+    //get and set next animation end points after an iteration
+    $thing.on("webkitAnimationIteration", function (e) {
+        var animName = e.originalEvent.animationName;
+        if (animName === "move") {
+            $thing.css({'top': position.y - 0.9 * $thing.width(), 'left': position.x - 0.5 * $thing.width()});
+        }
+        //sets angle of bird
+        opposite = newton($thing.offset().left, path) - newton($thing.offset().left + $thing.width(), path);
+        adjacent = $thing.width();
+        angle = -1 * Math.atan(opposite / adjacent) / Math.PI * 180;
+        position = shuffle($thing.offset().left + 0.5 * $thing.width(), 0.5 * $thing.width(), path, direction);
+        //set the css variables
+        document.documentElement.style.setProperty('--position-x', position.x - 0.5 * $thing.width());
+        document.documentElement.style.setProperty('--position-y', position.y - 0.9 * $thing.width());
+        document.documentElement.style.setProperty('--angle', angle + 'deg');
+    });
+}
+
+//turns off the animations after they've completed their current iteration
+function stopShuffling($thing, position, $birdBefore) {
+    //calls at start of iteration (except first one, kind of like fence post issue)
+    "use strict";
+    $thing.on("webkitAnimationIteration", function (e) {
+        var animName = e.originalEvent.animationName;
+        if (animName === "move") {
+            $thing.css({'top': position.y - 0.9 * $thing.width(), 'left': position.x - 0.5 * $thing.width()});
+        }
+        $thing.css('animation-play-state', 'paused');
+    });
+    $birdBefore.on("webkitAnimationIteration", function (e) {
+        $birdBefore.css('animation-play-state', 'paused');
+    });
+}
+
+$(document).ready(function () {
+    "use strict";
     var $bird = $("#bird"),
         $birdBefore = $('#bird div'),
-        path = document.getElementById("powerline");
+        path = document.getElementById("powerline"),
+        bird,
+        $mainNavLi,
+        _keyspressed;
     
 	//create a new state machine
-	var bird = new machina.Fsm({
+	bird = new machina.Fsm({
         namespace: "bird",
 		//any local variables and functions you'd like for your state machine
-        initialize: function (){
+        initialize: function () {
             _keyspressed = {
-                'left':false, //a key
-                'right':false //d key
+                'left': false, //a key
+                'right': false //d key
             };
-            var initialPosition = newton($(window).width()*0.8, path);
-            $bird.css({'top': (initialPosition -0.9*$bird.width()) + 'px', 'left':($(window).width()*0.8-0.5*$bird.width()) + 'px'}); 
+            var initialPosition = newton($(window).width() * 0.8, path);
+            $bird.css({'top': (initialPosition - 0.9 * $bird.width()) + 'px', 'left': ($(window).width() * 0.8 - 0.5 * $bird.width()) + 'px'});
             //console.log($bird.css('left'));
             //this is also where I think i'll set up my event listeners for the animations
             //also, initialize play-state
@@ -32,13 +160,13 @@ $(document).ready(function(){
 			//_onExit   ---- lets you define actions to take upen exiting a state
 			standing: {
 				//this state will react to both left and right keydown
-                _onEnter: function() {
+                _onEnter: function () {
                     //switch to standing animation (if there is one)
                 },
-				keydownleft: function() {
+				keydownleft: function () {
                     this.transition("shufflingLeft");
                 },
-				keydownright: function() {
+				keydownright: function () {
                     this.transition("shufflingRight");
                 }
 			},
@@ -48,13 +176,13 @@ $(document).ready(function(){
                     //use jquery animate for moving div, and css animation for sprite loops?
 					//after one cycle is complete, check if key is up
 					//if it is, pass the keyup action you've defined
-                    startShuffling($bird, "left");
+                    startShuffling($bird, "left", path, $birdBefore);
 				},
-                keydownright: function() {
+                keydownright: function () {
                     //instead of switching directly, emit event and add to queue?
                     this.deferAndTransition("standing");
                 },
-                keyupleft: function() {
+                keyupleft: function () {
                     console.log("kepupleft");
                     if (_keyspressed.right) {
                         this.handle("keydownright");
@@ -62,8 +190,8 @@ $(document).ready(function(){
                         this.transition("standing");
                     }
                 },
-                _onExit: function (){
-                    stopShuffling($bird, "left");
+                _onExit: function () {
+                    stopShuffling($bird, "left", $birdBefore);
                 }
 			},
             shuffleLeftInterrupt: {
@@ -75,13 +203,13 @@ $(document).ready(function(){
                     //use jquery animate for moving div, and css animation for sprite loops?
                     //after one cycle is complete, check if key is up
                     //if it is, pass the keyup action you've defined
-                    startShuffling($bird, "right")
+                    startShuffling($bird, "right", path, $birdBefore);
                 },
-                keydownleft: function() {
+                keydownleft: function () {
                     //this.clearQueue();
                     this.deferAndTransition("standing");
                 },
-                keyupright: function() {
+                keyupright: function () {
                     //this.clearQueue();
                     if (_keyspressed.left) {
                         this.handle("keydownleft");
@@ -89,8 +217,8 @@ $(document).ready(function(){
                         this.transition("standing");
                     }
                 },
-                _onExit: function (){
-                    stopShuffling($bird, "right");
+                _onExit: function () {
+                    stopShuffling($bird, "right", $birdBefore);
                 }
 			}
 		},
@@ -122,170 +250,56 @@ $(document).ready(function(){
 	});
     
 	//once the state machine is set up, here's where we'll be using it
-	$(document).keydown(function(e) {
-		e.preventDefault(); 
-        switch(e.which) {
+	$(document).keydown(function (e) {
+		e.preventDefault();
+        switch (e.which) {
             //controls for animation
             
-            case 65: // a (left)
+        case 65: // a (left)
             //this is where we'd like our bird to shuffle left
             bird.leftdown();
             
             break;
-            case 68: // d (right)
+        case 68: // d (right)
             //this is where we'd like our bird to shuffle right
             bird.rightdown();
             break;
 
-            default: return; 
+        default:
+            return;
         }
     
 	});
-	$(document).keyup(function(e) {
-		e.preventDefault(); 
-        switch(e.which) {
+	$(document).keyup(function (e) {
+		e.preventDefault();
+        switch (e.which) {
             //controls for animation
 
-            case 65: // a (left)
+        case 65: // a (left)
             //user releases the left key
             //bird should complete current cycle of animation, 
             //then go to standing then right if right key is down
             bird.leftup();
             break;
-            case 68: // d (right)
+        case 68: // d (right)
             //user releases the right key
             //bird should complete current cycle of animation, 
             //then go to standing then left if left key is down
             bird.rightup();
             break;
 
-            default: return; 
+        default:
+            return;
         }
 	});
-    
-    //given x, find y of given curve
-    function newton(x, path) {
-        //pick a starting point
-        //try halfway between x value and total length
-        //since length(x) >= x
-        var scale = $(path).parent('svg').width()/path.getBBox().width;
-         x /= scale;
-        var start = x,
-            end = path.getTotalLength(),
-            testlength,
-            testpoint;
-
-        //algorithm part
-        //edge cases: at end of path, at start of path
-        while(true) {
-          //try middle of current bounding region
-          testlength = Math.floor((start + end) / 2);
-          testpoint = path.getPointAtLength(testlength);
-          if (testlength === end || testlength === start) {
-              break;
-          } else if (testpoint.x > x) {
-            //guess was too high
-            end = testlength;
-
-          } else if (testpoint.x < x) {
-            //guess was too low
-            start = testlength;
-          } else {
-            //guess is correct
-            break;
-          }
-        }
-        return testpoint.y*scale;
-    }
-    //get position to shuffle to
-    function shuffle(x, step, path, direction) {
-        //move set distance (step) in specified direction
-        //parameters:
-        //x: current x position
-        //step: distance to shift
-        //path: curve to move along
-        //direction: which way to move along curve
-        //returns: 
-        //{x,y}: new position on curve
-        var newposition = {};
-        newposition.x = x;
-        //console.log("x is: " + x);
-        if (direction==="left") {
-          //new x value
-          newposition.x -= step;
-          //new y value
-        } else if (direction==="right") {
-          //new x value
-          newposition.x += step;
-          
-        } else {
-          //else throw error?d
-          //add one in later
-          console.log("error!");
-          return;
-        }
-        newposition.y = newton(newposition.x, path);
-        return newposition;
-    }
-
-    //start the shuffling animation in the specified direction
-    function startShuffling($thing, direction){
-        $thing.off("webkitAnimationIteration");
-        $birdBefore.off("webkitAnimationIteration");
-        //get next position on curve
-        var position = shuffle($thing.offset().left+ 0.5*$thing.width(), 0.5*$thing.width(), path, direction);
-        //find the slope of the curve at the birds current position
-        var angle, opposite, adjacent;
-        opposite = newton($thing.offset().left, path) - newton($thing.offset().left + $thing.width(), path);
-        adjacent = $thing.width();
-        angle = -1*Math.atan(opposite/adjacent)/Math.PI*180;
-        //set the css variables
-        document.documentElement.style.setProperty('--position-x', position.x-0.5*$thing.width())
-        document.documentElement.style.setProperty('--position-y', position.y - 0.9*$thing.width())
-        document.documentElement.style.setProperty('--angle', angle + 'deg');
-        //start the animations
-        $thing.css('animation-play-state','running');
-        $birdBefore.css('animation-play-state','running');
-        //get and set next animation end points after an iteration
-        $thing.on("webkitAnimationIteration", function(e) {
-            var animName = e.originalEvent.animationName;
-            if (animName === "move") {
-               $thing.css({'top': position.y -0.9*$thing.width(), 'left':position.x-0.5*$thing.width()}); 
-            }
-            //sets angle of bird
-            opposite = newton($thing.offset().left, path) - newton($thing.offset().left + $thing.width(), path);
-            adjacent = $thing.width();
-            angle = -1*Math.atan(opposite/adjacent)/Math.PI*180;
-            position = shuffle($thing.offset().left+0.5*$thing.width(), 0.5*$thing.width(), path, direction);
-            //set the css variables
-            document.documentElement.style.setProperty('--position-x', position.x-0.5*$thing.width());
-            document.documentElement.style.setProperty('--position-y', position.y - 0.9*$thing.width());
-            document.documentElement.style.setProperty('--angle', angle + 'deg');
-        });
-    }
-    
-    //turns off the animations after they've completed their current iteration
-    function stopShuffling($thing, position){
-        //calls at start of iteration (except first one, kind of like fence post issue)
-        $thing.on("webkitAnimationIteration", function(e) {
-            var animName = e.originalEvent.animationName;
-            if (animName === "move") {
-               $thing.css({'top': position.y - 0.9*$thing.width(), 'left':position.x-0.5*$thing.width()});  
-            }
-            $thing.css('animation-play-state','paused');
-        });
-        $birdBefore.on("webkitAnimationIteration", function(e) {
-            $birdBefore.css('animation-play-state','paused');
-        });
-    }
 
     
 	/* --------------------------------------------
 			     position main nav links on wire
 	--------------------------------------------- */
 	//get links containers
-	var $mainNavLi = $('.main-nav li');
-	$mainNavLi.each(function(){
-		$(this).css('top',newton($(this).offset().left, path) - $(window).width()/100);
+	$mainNavLi = $('.main-nav li');
+	$mainNavLi.each(function () {
+		$(this).css('top', newton($(this).offset().left, path) - $(window).width() / 100);
 	});
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,126 +1,131 @@
-$(document).ready(function(){
-    //set height of main element
-    //not currently using this, might get rid of later
-    var maxHeight = -1;
-   $('section').each(function() {
-     maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-   });
-    $('main').css('height', maxHeight + 200 + 'px');
+/*global $, console*/
+
+function navigate(origin, destination, angles, sections) {
+    "use strict";
+    var diff,
+        angle = 0,
+        theta;
+    //rotate here?
+    diff = sections[destination] - sections[origin];
+    // if differences is -, -1 or 3 rotate -90, -2 or 2, rotate 180, -3 or 1 rotate 90, 0 rotate 0
+    //instead of retrieving the angle, just change depending on what number is
+    //overview --starts at 0deg, html 90deg, scss 180deg, js -90deg
+    //current rotation will be sections.current * 90 mod 360
+    //new angle will be: (previous angle + new angle )%360
+    if (diff !== 0) {
+        $('section').removeClass('last');
+        $('section.' + origin).addClass('last');
+    }
+
+    if (diff === 0) {
+        //dont rotate
+        console.log("temp");
+    } else if (diff === -1 || diff === 3) {
+        //rotate -90 deg
+        angle += 90;
+    } else if (diff === 1 || diff === -3) {
+        //rotate 90 deg
+        angle -= 90;
+    } else if (Math.abs(diff) === 2) {
+        //rotate 180 deg
+        angle += 180;
+    } else {
+        //throw error: you forgot a case
+        console.log("ERRORS!");
+    }
+    //previous angle
+    for (theta in angles) {
+        angles[theta] += angle;
+        //figure out edges cases later so that this number doesgrow infintely
+        $('#' + theta).css('transform', 'translate(-50%, 0) rotateY(' + angles[theta] + 'deg)');
+    }
+    //remove active class from last one and add to current
+    //last one
+    $('section.active').toggleClass('active');
+
+    //current one
+    $('section.' + destination).toggleClass('active');
+}
+
+function copyToClipboard(elementId) {
+    
+    "use strict";
+    // Create a "hidden" input
+    var aux = document.createElement("input");
+
+    // Assign it the value of the specified element
+    aux.setAttribute("value", document.getElementById(elementId).innerText);
+
+    // Append it to the body
+    document.body.appendChild(aux);
+
+    // Highlight its content
+    aux.select();
+
+    // Copy the highlighted text
+    document.execCommand("copy");
+
+    // Remove it from the body
+    document.body.removeChild(aux);
+
+}
+
+$(document).ready(function () {
+    "use strict";
 
     //initialize to starting section
-    var sections = {'overview':0,'html':1,'scss':2,'js':3},
-        angles = {'overview':0, 'html':90, 'scss':180, 'js':270},
-        keys = ['overview','html','scss','js'];
+    var sections = {'overview': 0, 'html': 1, 'scss': 2, 'js': 3},
+        angles = {'overview': 0, 'html': 90, 'scss': 180, 'js': 270},
+        keys = ['overview', 'html', 'scss', 'js'];
 
-    function navigate(origin, destination) {
-        var diff,
-            angle = 0;
-        //rotate here?
-        diff = sections[destination]-sections[origin];
-        // if differences is -, -1 or 3 rotate -90, -2 or 2, rotate 180, -3 or 1 rotate 90, 0 rotate 0
-        //instead of retrieving the angle, just change depending on what number is
-        //overview --starts at 0deg, html 90deg, scss 180deg, js -90deg
-        //current rotation will be sections.current * 90 mod 360
-        //new angle will be: (previous angle + new angle )%360
-        if (diff!=0) {
-            $('section').removeClass('last');
-            $('section.' + origin).addClass('last');
-        }
-        
-        if (diff === 0) {
-            //dont rotate
-        } else if (diff===-1 || diff===3) {
-            //rotate -90 deg
-            angle +=90;
-        } else if (diff===1 || diff===-3) {
-            //rotate 90 deg
-            angle -=90;
-        } else if (Math.abs(diff)===2) {
-            //rotate 180 deg
-            angle +=180;
-        } else {
-            //throw error: you forgot a case
-            console.log("ERRORS!");
-        }
-        //previous angle
-        for (var theta in angles) {
-            angles[theta] += angle;
-            //figure out edges cases later so that this number doesgrow infintely
-            $('#' + theta).css('transform', 'translate(-50%, 0) rotateY(' + angles[theta] + 'deg)' );
-        }
-        //remove active class from last one and add to current
-        //last one
-        $('section.active').toggleClass('active');
-        
-        //current one
-        $('section.' + destination).toggleClass('active');
-    }
+    
     //events for clicking links
-    $('nav.main-nav a').on('click', function(e){
+    $('nav.main-nav a').on('click', function (e) {
         e.preventDefault();
         //get where rotating from and to
+        console.log("testing");
         var origin = $('section.active').attr('id'),
             destination = $(this).attr('href').substring(1);
-        navigate(origin, destination);
+        navigate(origin, destination, angles, sections);
         if ($('body').scrollTop() < $('.main-nav').offset().top) {
             $('body').animate({scrollTop: $('.main-nav').offset().top}, 500);
         }
     });
     //link to navigate to start of sections
-    $('.wrapper > span > a').on('click', function (e){
+    $('.wrapper > span > a').on('click', function (e) {
         e.preventDefault();
         $('body').animate({scrollTop: $('.main-nav').offset().top}, 500);
     });
-    $('section > span > a:first-of-type').on('click', function (e){
+    $('section > span > a:first-of-type').on('click', function (e) {
         e.preventDefault();
         $('body').animate({scrollTop: 0}, 500);
     });
     //events for arrow keys
-    $(document).keydown(function(e) {
-        var origin = $('section.active').attr('id');
-        switch(e.which) {
-            case 37: // left
+    $(document).keydown(function (e) {
+        var origin = $('section.active').attr('id'),
+            destination;
+        switch (e.which) {
+        case 37: // left
             //switch to previous page
-            destination = keys[(sections[origin] -1 + 4)%4];
-            navigate(origin,destination);
+            destination = keys[(sections[origin] - 1 + 4) % 4];
+            navigate(origin, destination, angles, sections);
             break;
 
-            case 38: // up
+        case 38: // up
             break;
 
-            case 39: // right
+        case 39: // right
             //switch to next page
-            destination = keys[(sections[origin] +1 + 4)%4];
-            navigate(origin,destination);
+            destination = keys[(sections[origin] + 1 + 4) % 4];
+            navigate(origin, destination, angles, sections);
             break;
 
-            case 40: // down
+        case 40: // down
             break;
-            default: return; // exit this handler for other keys
+        default:
+            return; // exit this handler for other keys
         }
         e.preventDefault(); // prevent the default action (scroll / move caret)
     });
 
 });
-
-function copyToClipboard(elementId) {
-
-  // Create a "hidden" input
-  var aux = document.createElement("input");
-
-  // Assign it the value of the specified element
-  aux.setAttribute("value", document.getElementById(elementId).innerText);
-
-  // Append it to the body
-  document.body.appendChild(aux);
-
-  // Highlight its content
-  aux.select();
-
-  // Copy the highlighted text
-  document.execCommand("copy");
-
-  // Remove it from the body
-  document.body.removeChild(aux);
-
-}
